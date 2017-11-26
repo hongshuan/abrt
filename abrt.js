1996ca2e9f443e4b52c3781ae356e78dd67bc469
Hongshuan Li <lihsca@gmail.com>
2017-11-26 12:47:13 -0500

    new design of page is done

--- abrt.js
+++ abrt.js
@@ -2,7 +2,8 @@ var timer;
 
 document.getElementById("start").addEventListener("click", start);
 document.getElementById("stop").addEventListener("click", stop);
-document.getElementById("clear").addEventListener("click", empty);
+document.getElementById("clear").addEventListener("click", clear);
+document.getElementById("empty").addEventListener("click", empty);
 
 function query() {
   var url = "http://www.play.dev/ttt.php";
@@ -34,15 +35,19 @@ function start() {
 }
 
 function stop() {
-    beep();
     clearInterval(timer);
     timer = null;
     println('Stop');
 }
 
-function empty() { document.getElementById("output").innerHTML = ''; }
+function clear() { document.getElementById("output").innerHTML = ''; }
 function print(text) { document.getElementById("output").innerHTML += text; }
 function println(text) { print(text + '<br>'); }
+
+function empty() { document.getElementById("messages").innerHTML = ''; }
+function show(text) { document.getElementById("messages").innerHTML += text; }
+function showln(text) { show(text + '<br>'); }
+
 function beep() { playSound('beep.wav'); }
 function sound() { playSound('NokiaEpic.mp3'); }
 function playSound(file) { var audio = new Audio('webres/' + file); audio.play(); }
