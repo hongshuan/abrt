ded8cc02d96fd2c737863ed9a593ac73ed94c8b6
Hongshuan Li <lihsca@gmail.com>
2018-05-11 18:52:13 -0400

    load adn save licenses

--- page/abrt.js
+++ page/abrt.js
@@ -13,6 +13,7 @@ function domReady() {
     getE("closeModal").addEventListener("click", closeModal);
 
     getE("search").addEventListener("keyup", licenseFilter);
+    getE("save").addEventListener("click", saveLicense);
 
     getE("tablink1").addEventListener("click", function(e) { openTab(e, "tab2") });
     getE("tablink2").addEventListener("click", function(e) { openTab(e, "tab1") });
@@ -102,6 +103,75 @@ function selectDriver(e) {
 
     getE("expiry").value = expiryDate;
     getE("licensenum").value = licenseNum;
+
+    closeModal();
+}
+
+function loadLicenses() {
+    var licenses = JSON.parse(localStorage.getItem("licenses"));
+
+    var html = `<tr>
+            <th>Name</th>
+            <th>License#</th>
+            <th>G2/G</th>
+            <th>Expiry</th>
+            <th>Action</th>
+            </tr>`;
+
+    for (var lic of licenses) {
+        html += `<tr>
+            <td>${lic.name}</td>
+            <td>${lic.licnum}</td>
+            <td>${lic.level}</td>
+            <td>${lic.expiry}</td>
+            <td><span class="w3-btn w3-tiny w3-blue">Copy</span></td>
+            </tr>`;
+    }
+
+    document.getElementById("mytable").innerHTML = html;
+}
+
+function saveLicense() {
+    var name = document.getElementById("name").value;
+    var licnum = document.getElementById("licnum").value;
+    var expire = document.getElementById("expire").value;
+    var level = document.querySelector('input[name="level"]:checked').value;
+
+    var licenses = JSON.parse(localStorage.getItem("licenses"));
+
+    if (!licenses) {
+        licenses = [
+            {
+                  name: "HE YANNAN",
+                licnum: "H2001-78909-46223",
+                 level: "G2",
+                expiry: "2022/01/06"
+            }, {
+                  name: "YU LINGHAO",
+                licnum: "Y9001-47409-31206",
+                 level: "G2",
+                expiry: "2022/01/06"
+            }, {
+                  name: "MENG YALING",
+                licnum: "M2531-78907-35113",
+                 level: "G",
+                expiry: "2018/05/29"
+            }, {
+                  name: "REN DA",
+                licnum: "R2492-14709-00720",
+                 level: "G",
+                expiry: "2022/07/23"
+            }, {
+                  name: "NIU QUNLI",
+                licnum: "N4741-63708-10415",
+                 level: "G2",
+                expiry: "2023/03/06"
+            }
+        ];
+    }
+
+    licenses.push({ name: name, licnum: licnum, expiry: expire, level: level });
+    localStorage.setItem("licenses", JSON.stringify(licenses));
 
     closeModal();
 }
